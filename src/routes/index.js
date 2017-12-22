b55c830a98667b688dc764bb2a93b8dce3e98c98
Nikolay <artamonov.nikolay@e-queo.com>
2017-12-22 11:26:08 +0300

    fix liter issues

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,4 +3,4 @@ import session from './session';
 
 const routes = [users, session];
 
-export default (router) => routes.forEach(route => route(router)) ;
+export default router => routes.forEach(route => route(router));
--- src/routes/session.js
+++ src/routes/session.js
@@ -1,14 +1,8 @@
 // @flow
-
-import encrypt from '../utilities/encrypt';
-import User from '../entyties/User';
-import UserRepository from '../repositories/UserRepository';
-
 export default (router) => {
-
-    router.get('/session', async (ctx) => {
-        delete ctx.session.user;
-        delete ctx.session.name;
-        ctx.redirect('/');
-    });
-}
+  router.get('/session', async (ctx) => {
+    delete ctx.session.user;
+    delete ctx.session.name;
+    ctx.redirect('/');
+  });
+};
