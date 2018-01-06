ad9f4e4150c670a0f8bcc20800d380ee0a4059d4
rende11 <artamonovn@gmail.com>
2018-01-06 23:58:44 +0300

    add postgres config

--- config/config.js
+++ config/config.js
@@ -9,6 +9,7 @@ module.exports = {
   },
   "production": {
     use_env_variable: "postgres://pcccwdobftjkho:2d2f64a0c325162db78bd152ba37af186185e24b12a8d5e79436eeb8a2b17b9e@ec2-107-22-183-40.compute-1.amazonaws.com:5432/db0sa6nfjsqvfk",
+    url: "postgres://pcccwdobftjkho:2d2f64a0c325162db78bd152ba37af186185e24b12a8d5e79436eeb8a2b17b9e@ec2-107-22-183-40.compute-1.amazonaws.com:5432/db0sa6nfjsqvfk",
     username: "pcccwdobftjkho",
     password: "2d2f64a0c325162db78bd152ba37af186185e24b12a8d5e79436eeb8a2b17b9e",
     database: "db0sa6nfjsqvfk",
