4f4e65899b935fc201cc0634a75a5968cdd6cf87
rende11 <artamonovn@gmail.com>
2018-01-07 10:23:21 +0300

    move pg settings to ENV

--- config/config.js
+++ config/config.js
@@ -8,12 +8,12 @@ module.exports = {
     dialect: 'sqlite'
   },
   "production": {
-    use_env_variable: "postgres://pcccwdobftjkho:2d2f64a0c325162db78bd152ba37af186185e24b12a8d5e79436eeb8a2b17b9e@ec2-107-22-183-40.compute-1.amazonaws.com:5432/db0sa6nfjsqvfk",
-    url: "postgres://pcccwdobftjkho:2d2f64a0c325162db78bd152ba37af186185e24b12a8d5e79436eeb8a2b17b9e@ec2-107-22-183-40.compute-1.amazonaws.com:5432/db0sa6nfjsqvfk",
-    username: "pcccwdobftjkho",
-    password: "2d2f64a0c325162db78bd152ba37af186185e24b12a8d5e79436eeb8a2b17b9e",
-    database: "db0sa6nfjsqvfk",
-    host: "ec2-107-22-183-40.compute-1.amazonaws.com",
+    use_env_variable: process.env.USER_ENV_VARIABLE,
+    url: process.env.URL,
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
     dialect: "postgres",
     port: "5432",
     dialectOptions: {
